40e9f4577a8c15e0d96388481eb79873b0316a47
dhavaljoshi008 <dhavaljoshi008@gmail.com>
2018-04-26 23:01:56 -0500

    feat(module-client): update cart service and component

--- jqmartclient/src/app/cart/cart.component.ts
+++ jqmartclient/src/app/cart/cart.component.ts
@@ -8,23 +8,44 @@ import { CartService } from './cart.service';
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
+
 export class CartComponent implements OnInit, OnDestroy {
 
-  cartMap: Map<string, Item> = new Map();
+  cartMap: Map<string, Item>;
+
+  subTotal: [number, number];
+
+  totalItemsInCart: number;
 
   modalTitle: string;
 
   modalBtnText: string;
 
+  labelText1: string;
+  labelText2: string;
+  labelText3: string;
+  labelText4: string;
+
   private subscription: Subscription;
+  private childSubscription1: Subscription;
+  private childSubscription2: Subscription;
 
   constructor(private cartService: CartService) {
+    this.cartMap = new Map<string, Item>();
+    this.totalItemsInCart = 0;
+    this.subTotal = [0, 0];
     this.modalTitle = 'Cart';
-    this.modalBtnText = 'Proceed To Checkout'
+    this.modalBtnText = 'View Cart and Checkout'
+    this.labelText1 = 'Total Items';
+    this.labelText2 = 'SubTotal';
+    this.labelText3 = 'Member';
+    this.labelText4 = 'Regular';
    }
 
   ngOnInit() {
     this.getCartItems();
+    this.getSubTotal();
+    this.getTotalItemsInCart();
   }
 
   getCartItems() {
@@ -34,6 +55,21 @@ export class CartComponent implements OnInit, OnDestroy {
 
   getCartMapKeys() {
       return Array.from(this.cartMap.keys());
+  }
+
+  getSubTotal() {
+    this.childSubscription2 = this.cartService.cartSubTotal
+    .subscribe(subTotal => this.subTotal = subTotal);
+  }
+
+  getTotalItemsInCart() {
+    this.childSubscription2 = this.cartService.totalItemsInCart
+    .subscribe(totalItemsInCart => this.totalItemsInCart = totalItemsInCart);
+    this.subscription.add(this.childSubscription2);
+  }
+
+  emptyCart() {
+    this.cartService.emptyCart();
   }
 
   ngOnDestroy() {
--- jqmartclient/src/app/cart/cart.service.ts
+++ jqmartclient/src/app/cart/cart.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Item } from '../inventory/item';
-import {Subject} from 'rxjs/Subject';
+import { Subject } from 'rxjs/Subject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
 
 
 @Injectable()
@@ -8,15 +10,19 @@ export class CartService {
 
   cartSubject = new Subject<Map<string, Item>>();
   cartMap: Map<string, Item> = new Map();
+
+  cartSubTotal = new BehaviorSubject<[number, number]>([0, 0]); // Member Price, Regular Price.
+  
+  totalItemsInCart = new BehaviorSubject<number>(0);
  
   constructor() {
 
   }
 
-  private addToCartMap(item: Item) {
+  private addToCartMap(item: Item): void {
     if(this.cartMap.has(item.id)) {
       let cartItem = this.cartMap.get(item.id);
-      cartItem.quantity += 1; // Increment quantity of existing item in cart by 1.
+      cartItem.quantity += 1; // Increment quantity of existing item in the cart by 1.
       this.cartMap.set(item.id, cartItem);
     }
     else {
@@ -24,8 +30,38 @@ export class CartService {
       this.cartMap.set(item.id, item);
     }
   }
-  addToCart(item: Item) {
+  
+  // Add item to the cart.
+  addToCart(item: Item): void {
     this.addToCartMap(item);
     this.cartSubject.next(this.cartMap);
+    this.calculateSubTotal();
+    this.calculateTotalItemsInCart();
+  }
+
+  // Calculate member and regular sub-total.
+  calculateSubTotal(): void {
+    let subTotal:[number, number] = [0, 0];
+    this.cartMap.forEach(item => {
+      subTotal[0] += item.memberPrice * item.quantity;
+      subTotal[1] += item.regularPrice * item.quantity;
+    });
+    this.cartSubTotal.next(subTotal);
+  }
+
+  // Calculate total items in the cart.
+  calculateTotalItemsInCart(): void {
+    let totalItems = 0;
+    this.cartMap.forEach(item => {
+      totalItems += item.quantity;
+    });
+    this.totalItemsInCart.next(totalItems);
+  }
+
+  emptyCart(): void {
+    this.cartMap.clear();
+    this.cartSubject.next(this.cartMap);
+    this.calculateSubTotal();
+    this.calculateTotalItemsInCart();
   }
 }
